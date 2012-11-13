5dfa4defe4983e04656fc30c672bf43decb8486b
Brian McCallister <brianm@skife.org>
2012-11-12 21:59:27 -0700

    some docs

--- smav.go
+++ smav.go
@@ -1,3 +1,10 @@
+/* 
+
+ Package variant provides some (well, at the time of this writing)
+ useful implementations of the expvar.Var interface (which is just a
+ Stringer).
+
+*/
 package variant
 
 import (
@@ -6,12 +13,17 @@ import (
 	"sync"
 	"container/ring")
 
+// represents a size bounded simple moving average
+// it is thread/goroutine safe
 type SimpleMovingAverage struct {
 	size int
 	mutex *sync.Mutex
 	values *ring.Ring
 }
 
+// Crate a new simple moving average expvar.Var. It will be
+// published under `name` and maintain `size` values for
+// calculating the average.
 func NewSimpleMovingAverage(name string, size int) *SimpleMovingAverage {
 	sma := new(SimpleMovingAverage)
 	sma.size = size
@@ -24,10 +36,12 @@ func NewSimpleMovingAverage(name string, size int) *SimpleMovingAverage {
 	return sma
 }
 
+// display the value as a string
 func (s *SimpleMovingAverage) String() string {
 	return fmt.Sprintf("%f", s.Average())
 }
 
+// Append a new value to the stat
 func (s *SimpleMovingAverage) Update(val float64) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
@@ -36,6 +50,7 @@ func (s *SimpleMovingAverage) Update(val float64) {
 	s.values = s.values.Next()
 }
 
+// obtain the current value
 func (s *SimpleMovingAverage) Average() float64 {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
